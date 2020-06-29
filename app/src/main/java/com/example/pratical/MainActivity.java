d41fde4c19b74954da3a9718569d844e98361d4a
neijunxian <neijx-wm19@student.tarc.edu.my>
2020-06-29 22:07:14 +0800

    commit by nei jun xian

---
+++ app/src/main/java/com/example/pratical/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.pratical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount =(TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast= Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if(mShowCount !=null) {
+            mShowCount.setText(Integer.toString(mCount));
+            view.setBackgroundColor(Color.GREEN);
+            findViewById(R.id.button_zero).setBackgroundColor(Color.RED);
+
+        }
+    }
+
+    public void countZero(View view) {
+        mCount=0;
+        mShowCount.setText(Integer.toString(mCount));
+        findViewById(R.id.button_zero).setBackgroundColor(Color.GRAY);
+    }
+}
